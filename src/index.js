0f11ea23ca5628538e48daddcc4ebadecca963c8
BOISSE Maxime <m.boisse@groupeonepoint.com>
2019-08-13 10:28:23 +0200

    add firebase, react router and redux

--- src/index.js
+++ src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
+const config = {
+    apiKey: "AIzaSyA_9Jp98wwTpT3j_7a_1Kr2sgRE3NWDDaM",
+    authDomain: "kanaflash-adcb1.firebaseapp.com",
+    databaseURL: "https://kanaflash-adcb1.firebaseio.com",
+    projectId: "kanaflash-adcb1",
+    storageBucket: "kanaflash-adcb1.appspot.com",
+    messagingSenderId: "1041831295777",
+    appId: "1:1041831295777:web:fa470310a351eb82"
+};
+firebase.initializeApp(config);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
