e79a947c3e95d59d0a1877a4d3d161048a303f30
BOISSE Maxime <m.boisse@groupeonepoint.com>
2019-08-13 17:22:14 +0200

    add navigation through the app

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
+import {Link} from 'react-router-dom';
+
 import './Header.scss';
 
 class Header extends Component {
     render(){
         return (
-            <header className="header">Header</header>
+            <header className="header">
+                <Link to="/">Home</Link>
+            </header>
         )
     }
 }
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
     render(){
-        return <h1> Hello world </h1>;
+        return (
+            <div>
+                <button><Link to = "/katakana"> Katakana </Link></button>
+                <button><Link to = "/hiragana"> Hiragana </Link></button>           
+            </div>
+        );
     }
 }
 
