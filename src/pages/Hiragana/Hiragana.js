099740d08e73d27ced3c69d9d995892fea84665d
Maxime <maxime.boisse@protonmail.com>
2019-08-13 23:27:11 +0200

    add hiragana random picking

--- src/pages/Hiragana/Hiragana.js
+++ src/pages/Hiragana/Hiragana.js
@@ -1,11 +1,32 @@
 import React, {
     Component
 } from 'react';
+import { directive } from '@babel/types';
+const hiraganas = require('../../assets/kana/hiragana.json');
 
-class Katakana extends Component {
+class Hiragana extends Component {
+  constructor(props){
+    super(props);
+    this.randomHiragana = this.randomHiragana.bind(this);
+    this.state = { randomHiraganas: 0}
+  }
+  
+  randomHiragana(object) {
+    const max_number = Math.ceil(Object.keys(object).length);
+    const rand_key = Math.floor(Math.random() * (max_number));
+    return this.setState({randomHiraganas: rand_key});
+
+  }
+
     render() {
-        return <h1> Hiragana page </h1>
+      const {randomHiraganas} = this.state;
+      return(
+          <section>
+            {Object.values(hiraganas)[randomHiraganas]}
+            <button onClick={this.randomHiragana}>Refresh</button>
+          </section>
+        )
     }
 }
 
-export default Katakana;
+export default Hiragana;
