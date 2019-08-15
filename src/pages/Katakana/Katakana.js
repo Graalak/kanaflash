f0d1ae85dd20cf3b7998a8dda2e5a3325f575a45
Maxime <maxime.boisse@protonmail.com>
2019-08-15 15:25:48 +0200

    add the katakana part

--- src/pages/Katakana/Katakana.js
+++ src/pages/Katakana/Katakana.js
@@ -1,8 +1,32 @@
-import React, {Component} from 'react';
+import React, {
+    Component
+} from 'react';
+import './Katakana.scss';
+import Answer from '../../components/Answer/Answer';
+const katakanas = require('../../assets/kana/katakana.json');
 
 class Katakana extends Component {
-    render(){
-        return <h1>Katakana page</h1>
+    constructor(props){
+        super(props);
+        this.randomKatakana = this.randomKatakana.bind(this);
+        this.max_number = Math.ceil(Object.keys(katakanas).length);
+        let rand_key = Math.floor(Math.random() * (this.max_number));
+        this.state = { randomKatakanas: rand_key};
+    }
+
+    randomKatakana() {
+    let rand_key = Math.floor(Math.random() * (this.max_number));
+    return this.setState({randomKatakanas: rand_key});
+    }
+
+    render() {
+        const {randomKatakanas} = this.state;
+        return(
+            <section>
+                <p className="kana">{Object.values(katakanas)[randomKatakanas]}</p>
+                <Answer kana_trad={Object.keys(katakanas)[randomKatakanas]} randomFunction={this.randomKatakana}/>
+            </section>
+        )
     }
 }
 
